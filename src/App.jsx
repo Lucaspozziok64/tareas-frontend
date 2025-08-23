3fb21e33990e16a770c6e0343f46873868b3a76d
Lucasfi <lukafigueroa64@gmail.com>
2025-08-22 21:21:29 -0300

    agregue gradiente de fondo a la app y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FormularioTareas from './components/FormularioTareas'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
+    <main>
       <h1 className='text-center my-5'>Aplicacion Tareas</h1>
       <section className='container contenedorForm py-2 bg-primary rounded-3'>
         <FormularioTareas />
       </section>
+    </main>
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="py-1 bg-black d-flex justify-content-center align-items-center">
+            <p className="text-center mb-0">Desarrolado por <a href="https://github.com/Lucaspozziok64">Lucas Figueroa</a>👨‍💻</p>
+        </footer>
+    );
+};
+
+export default Footer;
